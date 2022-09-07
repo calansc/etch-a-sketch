4ee65ac35cffc00d06673c85ac879479a40baed5
Chad Schroeder <caschroeder@gmail.com>
2022-09-07 13:18:27 -0400

    Step 3 instructions in script.js

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function createSquares() {
         square.textContent='test'+i;
     }
 };
+
 createSquares()
 const squares = document.getElementsByClassName('squares');
 console.log(squares.length, typeof squares);
+
+
+//Step 3 - set up hover effect so that grid divs change color when mouse passes over them, leaving a trail through your grid. Add a new class to color or change div's background color with js.
