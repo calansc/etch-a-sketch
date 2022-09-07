f09d8200f2f75c63cfe22d94e3d3184359c925ca
Chad Schroeder <caschroeder@gmail.com>
2022-09-07 12:47:23 -0400

    Change grid to 16x16

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const container = document.querySelector('#container');
 
 function createSquares() {
-    for (let i=0; i <16; i++) {
+    for (let i=0; i <256; i++) {
         const square = document.createElement('div');
         container.appendChild(square);
         square.setAttribute('class','squares');
