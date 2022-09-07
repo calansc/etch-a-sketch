3fb5ff2a9fd744f8b863b3c7938f99a4c1074eef
Chad Schroeder <caschroeder@gmail.com>
2022-09-07 16:50:53 -0400

    Add mouseenter highlight for squares
    Add top button for custom side length prompt

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 
 const container = document.querySelector('#container');
+let side = 16;
+let sideSq = side*side;
 
 function createSquares() {
-    for (let i=0; i <256; i++) {
+    for (let i=0; i < sideSq; i++) {
         const square = document.createElement('div');
         container.appendChild(square);
         square.setAttribute('class','squares');
         square.textContent='test'+i;
+        square.setAttribute('id',i);
+        square.style.width = '40px';
+        square.style.height = '40px';
+        square.addEventListener('mouseenter', () => {
+            square.style.background = 'gray';
+        })
     }
 };
+
+function sideLengthButton() {
+    let side = prompt('Enter Desired Side Length (Max 100)', 16);
+    console.log(side);
+    //return(side);
+}
 
 createSquares()
 const squares = document.getElementsByClassName('squares');
 console.log(squares.length, typeof squares);
 
 
-//Step 3 - set up hover effect so that grid divs change color when mouse passes over them, leaving a trail through your grid. Add a new class to color or change div's background color with js.
+
+//Step 4 - let user adjust squares per side. 
+//Created button to edit with default 16.
+//How to recreate table
+//How to change square div size
