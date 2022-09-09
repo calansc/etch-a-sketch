d0902127082da016ca4e2a320f3c1962c7ed415e
Chad Schroeder <caschroeder@gmail.com>
2022-09-09 15:20:15 -0400

    Add customizable table
    Add check that input is between 1-100.
    Add div deletion for grid recreation.
    Remove test textContent.

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 const container = document.querySelector('#container');
 let side = 16;
 let sideSq = side*side;
+let sqWidth = (640/side).toString()+'px';
+//console.log(sqWidth, typeof sqWidth);
 
 function createSquares() {
     for (let i=0; i < sideSq; i++) {
         const square = document.createElement('div');
         container.appendChild(square);
         square.setAttribute('class','squares');
-        square.textContent='test'+i;
+        //square.textContent='test'+i;
         square.setAttribute('id',i);
-        square.style.width = '40px';
-        square.style.height = '40px';
+        square.style.width = sqWidth;
+        square.style.height = sqWidth;
         square.addEventListener('mouseenter', () => {
             square.style.background = 'gray';
         })
@@ -19,14 +21,26 @@ function createSquares() {
 };
 
 function sideLengthButton() {
-    let side = prompt('Enter Desired Side Length (Max 100)', 16);
-    console.log(side);
-    //return(side);
+    let sideLength = prompt('Enter Desired Side Length (1-100)', 16);
+    if (typeof sideLength !== 'string' ||sideLength > 100 || sideLength < 1 || 
+    sideLength - 1 !== sideLength -1) {
+        alert('Please enter a number between 1 and 100');
+        return;
+    }
+    side = sideLength;
+    sideSq = side*side;
+    sqWidth = (640/side).toString()+'px';
+    //console.log(side);
+    const squareDelete = document.getElementById('container');
+    while (squareDelete.firstChild) {
+        squareDelete.removeChild(squareDelete.lastChild);
+    }
+    createSquares();
 }
 
 createSquares()
 const squares = document.getElementsByClassName('squares');
-console.log(squares.length, typeof squares);
+//console.log(squares.length, typeof squares);
 
 
 
